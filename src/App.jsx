e8b9d457b0d9f59ad04d76c44c21da692282042e
adityam0105 <adityamukherji405@gmail.com>
2024-03-09 17:54:40 +0530

    Implement the dark theme functionality using tailwind CSS

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import './App.css'
+import Home from './Pages/Home'
+import Header from './Components/Header'
+import { useEffect, useState } from 'react'
+import { ThemeContext } from './Context/ThemeContext'
+
+function App() {
+  const [theme,setTheme]=useState('light')
+  useEffect(()=>{
+    setTheme(localStorage.getItem('theme')?localStorage.getItem('theme'):'dark')
+  },[])
+  return (
+    <>
+      <ThemeContext.Provider value={{theme,setTheme}}>
+      <div className= {`${theme} ${theme=='dark'?'bg-[#31363F]':'bg-[#EEEEEE]'} h-[100vh]`}>
+      <Header></Header>
+      <Home></Home>
+      </div>
+      </ThemeContext.Provider>
+      
+      
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/Pages/Home.jsx
@@ -0,0 +1,13 @@
+const Home = () => {
+  return (
+
+    <div>
+      <div>Genre</div>
+      <div>Game List</div>
+    </div>
+    
+
+  )
+}
+
+export default Home
