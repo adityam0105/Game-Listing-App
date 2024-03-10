9b6866161a628a890d3d4769d4874f2a7ae3012a
adityam0105 <adityamukherji405@gmail.com>
2024-03-10 16:03:24 +0530

    Implemented the API key logic using rawg API for  steam

---
+++ src/Services/GlobalApi.jsx
@@ -0,0 +1,12 @@
+import axios from 'axios'
+const axiosCreate=axios.create({
+    baseURL : 'https://api.rawg.io/api'
+})
+const API_KEY='494b131630014e44a8da15b718c346b0';
+if (!API_KEY) {
+    throw new Error('API_KEY is not provided in the environment variables');
+}
+const getGenreList =axiosCreate.get('/genres?key='+API_KEY);
+export default{
+    getGenreList,
+}
