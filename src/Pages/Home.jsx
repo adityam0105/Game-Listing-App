0d40072fbe15cf28bfd3adc518fcc08ff8d4e7d3
adityam0105 <adityamukherji405@gmail.com>
2024-03-10 18:43:35 +0530

    Integrated the Home Page with different components

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,13 @@
+import GenreList from "../Components/GenreList"
+
 const Home = () => {
   return (
 
-    <div>
-      <div>Genre</div>
-      <div>Game List</div>
+    <div className="grid grid-cols-4">
+      <div className="h-full hidden md:block px-5">
+        <GenreList />
+      </div>
+      <div className=" bg-blue-500 col pt-3 col-span-4 md:col-span-3 dark:text-white">Game List</div>
     </div>
     
 
