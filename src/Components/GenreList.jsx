a38f3d2d8df3963f6232f7066719c87dfd70ace4
adityam0105 <adityamukherji405@gmail.com>
2024-03-10 18:42:37 +0530

    Improved styling of the component and added the redering of game icons and names

--- src/Components/GenreList.jsx
+++ src/Components/GenreList.jsx
@@ -1,15 +1,33 @@
-import { useEffect } from 'react'
-import GlobalApi from '../Services/GlobalApi'
+import { useEffect, useState } from "react";
+import GlobalApi from "../Services/GlobalApi";
 
 const GenreList = () => {
-  useEffect(()=>{
-    const fetchData = async ()=>{
-        const res =await GlobalApi.getGenreList;
-        console.log(res)
-    }
-    fetchData();
-  },[])
-  
-}
+  const [genreList, setGenreList] = useState([]);
+  useEffect(() => {
+    getGenreList();
+  }, []);
+  const getGenreList = () => {
+    GlobalApi.getGenreList.then((res) => {
+      console.log(res.data.results);
+      setGenreList(res.data.results);
+    });
+  };
+  return (
+    <div>
+      <h2 className="text-[30px] font-bold dark:text-white">Genre</h2>
+      {genreList.map((item) => (
+        <div className="flex gap-2 items-center mb-2 cursor-pointer hover:bg-gray-300 p-2 rounded-lg group">
+          <img
+            src={item.image_background}
+            className="h-[60px] w-[60px] object-cover rounded-lg group-hover:scale-105 transition-all ease-out duration-200"
+          />
+          <h3 className="hover:dark:text-black dark:text-white hover:text-gray-700 text-[18px] group-hover:font-bold transition-all ease-out duration-200">
+            {item.name}
+          </h3>
+        </div>
+      ))}
+    </div>
+  );
+};
 
-export default GenreList
+export default GenreList;
