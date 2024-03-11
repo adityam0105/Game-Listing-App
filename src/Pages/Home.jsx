45244a736a968148f3839ef9977c3dd009356f49
adityam0105 <adityamukherji405@gmail.com>
2024-03-11 20:32:42 +0530

    Integreted all the components integrated onto the Home Page

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,16 +1,40 @@
+import { useEffect, useState } from "react"
 import GenreList from "../Components/GenreList"
+import GlobalApi from "../Services/GlobalApi";
+import Banner from "../Components/Banner";
+import TrendingGames from "../Components/TrendingGames";
+import GamesbyGenereId from "../Components/GamesbyGenereId";
 
 const Home = () => {
+  const [allGameList,setAllGamesList]=useState();
+  const [gameListbyGenres,setGameListByGenres]=useState([])
+  useEffect(()=>{
+    getAllGamesList();
+    getGamesListByGeneresId();
+  },[]) 
+  const getAllGamesList =()=>{
+    GlobalApi.getAllGames.then((res)=>{
+      setAllGamesList(res.data.results);
+      setGameListByGenres(res.data.results);
+    })
+  }
+  const getGamesListByGeneresId= (id)=>{
+    GlobalApi.getGameListByGenreId(4).then((res)=>{
+      console.log("Inside getGames by genre",res.data.results)
+    })
+  }
   return (
-
-    <div className="grid grid-cols-4">
-      <div className="h-full hidden md:block px-5">
+    <div className="grid grid-cols-4 px-8">
+      <div className="hidden md:block">
         <GenreList />
       </div>
-      <div className=" bg-blue-500 col pt-3 col-span-4 md:col-span-3 dark:text-white">Game List</div>
+      { allGameList?.length>0?
+      <div className="md:col-span-3 col-span-4 px-3">
+        <Banner gameBanner={allGameList[0] } />
+        <TrendingGames gameList={allGameList}/>
+        <GamesbyGenereId gameList={getGamesListByGeneresId}/>
+      </div>: null}
     </div>
-    
-
   )
 }
 
